5eb88b6d08db2a94ee5b4d82533fcee39382279a
rewinn <rachel.winn@me.com>
2015-05-20 18:06:18 -0500

    Desktop Complete
    - fixed top nav scroll issue
    - changed font size

--- js/index.js
+++ js/index.js
@@ -1,52 +1,47 @@
 $(document).ready(function() {
-  //$("img").unveil();
-  
-// Closes the sidebar menu
-$("#menu-close").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
+
+    // Closes the sidebar menu
+    $("#menu-close").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
 
 
-// Opens the sidebar menu
-$("#menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-});
+    // Opens the sidebar menu
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+    });
 
-// Scrolls to the selected menu item on the page
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
-
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 1000);
-                return false;
+    // Scrolls to the selected menu item on the page
+    $(function() {
+        $('a[href*=#]:not([href=#])').click(function() {
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html,body').animate({
+                        scrollTop: target.offset().top - 70
+                    }, 1000);
+                    return false;
+                }
             }
-        }
+        });
     });
 });
 
 
-// This adds an offset in case the header is fixed
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-                var top_offset = $('div.navbar-inverse').height();
-                if ( $('.site-header').css('position') == 'fixed' ) {
-                    top_offset = $('.site-header').height();
-                }
-                 $('html,body').animate({
-                     scrollTop: target.offset().top - top_offset
-                }, 1000);
-                return false;
-            }
-        }
-    }); 
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
